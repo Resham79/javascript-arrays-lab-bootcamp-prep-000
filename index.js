3e42faf9df9a0e372112936f277cbe1e030be001
Resham Arora <resham.arora@gmail.com>
2019-04-01 23:13:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  return 
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(0)
 }
